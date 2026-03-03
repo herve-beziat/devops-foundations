8c6e2ee8b025b460b3c84cfd3ea3c4dc1ad352a7
herve-beziat <herve.beziat@laplateforme.io>
2026-03-03 12:04:52 +0100

    feat(backend): add postgres connectivity check endpoint

--- src/backend/src/index.js
+++ src/backend/src/index.js
@@ -14,6 +14,7 @@ const PORT = process.env.PORT || 3000;
 // Import des routes (modulaires)
 const healthRouter = require('./routes/health');
 const rootRouter = require('./routes/root');
+const dbRouteur = require('./routes/db');
 
 // Montage des routes :
 // ici, toutes les routes définies dans health.js seront accessibles sous /health
@@ -21,6 +22,8 @@ const rootRouter = require('./routes/root');
 app.use('/health', healthRouter);
 // Monter la route racine
 app.use('/', rootRouter);
+// Monter la route de test de la base de données
+app.use('/db', dbRouteur);
 
 // Démarrage du serveur
 // Le serveur écoute les requêtes HTTP sur le port défini
